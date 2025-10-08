589b9549dfe565196b31efba85d3e338bec272bf
Joseeg17 <lsbjosemanuelguerrero@gmail.com>
2025-10-08 12:24:25 +0000

    crea proyecto hola mundo

---
+++ demo/src/test/java/com/ingesoft/universidad/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingesoft.universidad.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
